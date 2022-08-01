184962883a4d4d330ad931bebb0bc6955887cea9
Romaric Pascal <romaric.pascal@amba.co>
2022-08-01 17:16:01 +0100

    Update path to helper

--- lib/merge_attributes/railtie.rb
+++ lib/merge_attributes/railtie.rb
@@ -3,8 +3,8 @@ module MergeAttributes
   class Railtie < ::Rails::Railtie
     initializer "merge_attributes.action_view" do |app|
       ActiveSupport.on_load :action_view do
-        require "merge_attributes/helpers/merge_attributes_helper"
-        include MergeAttributes::Helpers::MergeAttributesHelper
+        require "merge_attributes/helper"
+        include MergeAttributes::Helper
       end
     end
   end
