6aa279d67bbe8e9c0c8e5b990f7e22471d31cbae
Romaric Pascal <romaric.pascal@amba.co>
2022-08-01 17:14:20 +0100

    Fix poor copy-paste for loading the Railtie

--- lib/merge_attributes.rb
+++ lib/merge_attributes.rb
@@ -3,4 +3,4 @@ require "merge_attributes/helper"
 
 # If running inside a rails app, inject the helper
 # https://api.rubyonrails.org/classes/Rails/Railtie.html
-require "my_gem/railtie" if defined?(Rails::Railtie)
+require "merge_attributes/railtie" if defined?(Rails::Railtie)
